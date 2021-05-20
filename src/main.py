377154ee5dec7403c3322473ff6997e10a8f6fcc
xanx1 <vsanchez.gassull@gmail.com>
2021-05-20 14:28:15 +0200

    add python base code structure

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import argparse
+
+"""
+LZ4 SEQUENCE
+
+1-byte      token
+0-n bytes   literal-length
+0-L bytes   literals
+2-bytes     offset
+0-n bytes   match-length
+"""
+
+def hex2bin(hex_value) -> str:
+    return bin(int(hex_value, 16)).zfill(8).split('b')[1]
+
+def bin2hex(bin_value) -> str:
+    return hex(int(bin_value, 2))[2:] 
+
+def config_args():
+    parser = argparse.ArgumentParser(description="lz4 algorithm to compress and deflate files")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--c', '--compress', action='store_true')
+    group.add_argument('--d', '--deflate', action='store_false')
+    parser.add_argument('file')
+    return parser.parse_args()
+
+def main():
+    print("uwu")
+
+
+if __name__ == '__main__':
+    args = config_args()
+    if args.c:
+        print(hex2bin(args.file))
+        # compress(args.file)
+    else:
+        print(bin2hex(args.file))
+        # deflate(args.file)
+    
+
