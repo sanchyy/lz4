c922881e110f9c7cd5bbd1bcb4ca2a47e51b2ed3
xanx1 <vsanchez.gassull@gmail.com>
2021-05-25 19:25:52 +0200

    change typo deflate --> decompress

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time 
 
 """
 LZ4 SEQUENCE
@@ -14,20 +15,19 @@ m -> tamany del match
 
 n = 8192
 m = 128
-(recommended)
 """
 
 def hex2bin(hex_value) -> str:
     return bin(int(hex_value, 16)).zfill(8).split('b')[1]
 
 def bin2hex(bin_value) -> str:
-    return hex(int(bin_value, 2))[2:] 
+    return hex(int(bin_value, 2))[2:]
 
 def config_args():
     parser = argparse.ArgumentParser(description="lz4 algorithm to compress and deflate files")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--c', '--compress', action='store_true')
-    group.add_argument('--d', '--deflate', action='store_false')
+    group.add_argument('--d', '--decompress', action='store_false')
     parser.add_argument('file')
     return parser.parse_args()
 
@@ -37,6 +37,11 @@ def read_file(path_file):
     f.close()
     return res
 
+def decompress(filename):
+    data = read_file(filename)
+    for c in data:
+
+
 def main():
     args = config_args()
     if args.c:
@@ -44,7 +49,7 @@ def main():
         # compress(args.file)
     else:
         print(bin2hex(args.file))
-        # deflate(args.file)
+        # decompress(args.file)
 
 
 if __name__ == '__main__':
